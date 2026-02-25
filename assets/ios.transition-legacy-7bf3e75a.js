db33471e0be02da522c8eeb3bd22aca10b76b941
bryan-melanson <bryan-melanson@users.noreply.github.com>
2026-02-25 02:23:44 +0000

    deploy: 20cd9778f1f29aefd0ea3bc3f44eedd6ce245796

--- assets/ios.transition-legacy-7bf3e75a.js
+++ assets/ios.transition-legacy-7bf3e75a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fd3fd783.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.b,n=t.g}],execute:function(){
+System.register(["./index-legacy-783670a1.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.b,n=t.g}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
